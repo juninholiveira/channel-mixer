7a6ac8cac8a869c0893f0b5199ffab2124c5b3fa
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-07-21 14:18:21 -0300

    feat: use native titlebar

--- public/electron.js
+++ public/electron.js
@@ -8,15 +8,18 @@ function createWindow() {
 		width: 800,
 		height: 600,
 		backgroundColor: "#1E1E1E",
-		frame: false,
-		fullscreen: false,
-		fullscreenable: false,
+		//frame: true,
+		//fullscreen: false,
+		//fullscreenable: false,
 		webPreferences: {
 			nodeIntegration: false,
 			contextIsolation: true,
 			preload: path.join(__dirname, "preload.js"),
 		},
 	})
+
+	mainWindow.setMenuBarVisibility(false)
+	mainWindow.setMaximizable(false)
 
 	// Load index.html
 	mainWindow.loadURL(
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import { TitleBar } from "react-desktop/windows"
-
 export default function App() {
 	return (
 		<div>
-			<TitleBar
-				title="Channel Mixer"
-				controls={true}
-				isMaximized={true}
-				theme={"light"}
-				background={"#CFF465"}
-				onCloseClick={() => {console.log("close")}}
-				onMinimizeClick={() => {console.log("minimize")}}
-				onMaximizeClick={() => {console.log("maximize")}}
-				onRestoreDownClick={() => {console.log("restoreDownClick")}}
-			/>
 			<h1>channel-mixer</h1>
 		</div>
 	)
