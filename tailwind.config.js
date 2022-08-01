9d5e39eb81f23d807f30fa3420471ba33d0c8902
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-08-01 20:45:27 -0300

    feat: add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
 	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
 		extend: {},
+		colors: {
+			"light-accent": "#CFF465",
+			"light-background": "#1E1E1E",
+			"dark-accent": "#4E683E",
+			"dark-background": "#EFEFEF",
+		},
 	},
 	plugins: [],
 }
