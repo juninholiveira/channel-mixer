ff36e001163228b04b11bc1541d3421574d18cb6
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-08-01 20:45:38 -0300

    feat: add MixButton

---
+++ src/components/MixButton.tsx
@@ -0,0 +1,7 @@
+export default function MixButton() {
+	return (
+		<button onClick={() => console.log("Mix Clicked")} className="bg-light-accent text-lg text-light-background rounded-md w-16 h-full">
+				MIX
+		</button>
+	)
+}
