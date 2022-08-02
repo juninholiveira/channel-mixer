aff6ed529bcd2448b00bafd1a0b5d3727f008161
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-08-02 01:02:36 -0300

    feat: hover gradient

--- src/components/MixButton.tsx
+++ src/components/MixButton.tsx
@@ -1,6 +1,6 @@
 export default function MixButton() {
 	return (
-		<button onClick={() => console.log("Mix Clicked")} className="bg-light-accent text-lg text-light-background rounded-md w-16 h-full">
+		<button onClick={() => console.log("Mix Clicked")} className="bg-light-accent text-lg text-light-background rounded-md w-16 h-full hover:bg-gradient-to-tr hover:from-yellow-400 hover:via-red-500 hover:to-purple-500 hover:text-white">
 				MIX
 		</button>
 	)
