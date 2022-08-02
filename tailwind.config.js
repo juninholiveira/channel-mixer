6a42b8c50440ca8e073ec07d44f38c481d423fcf
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-08-01 23:30:44 -0300

    fix: extend instead of replace

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
 	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
-		colors: {
-			"light-accent": "#CFF465",
-			"light-background": "#1E1E1E",
-			"dark-accent": "#4E683E",
-			"dark-background": "#EFEFEF",
-		},
+		extend: {
+			colors: {
+				"light-accent": "#CFF465",
+				"light-background": "#1E1E1E",
+				"dark-accent": "#4E683E",
+				"dark-background": "#EFEFEF",
+			}},
+
 	},
 	plugins: [],
 }
