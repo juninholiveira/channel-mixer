7ff2ea66e92a1719cf03cd8b1af571842c3eea9c
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-08-02 21:56:26 -0300

    feat: switch polish

--- src/components/InputChannel.tsx
+++ src/components/InputChannel.tsx
@@ -92,7 +92,7 @@ export default function InputChannel({ channel }:IInputChannelProps) {
 						onClick={() => {
 							handleSwitch()
 						}}
-						className="w-9 h-5 m-0 bg-light-background rounded-sm
+						className="w-9 h-5 m-0 bg-light-background rounded-md
 						peer peer-checked:after:translate-x-full
 						after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-light-accent after:border after:rounded-sm after:h-4 after:w-4 after:transition-all"
 					></div>
