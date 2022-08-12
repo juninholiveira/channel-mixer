119f50317280a946c71496d5f5e2f2b5da7f69cc
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-08-12 00:19:49 -0300

    feat: possibility to deactivate the alpha channel (#17)

---
+++ src/components/DeactivatedInputContainer.tsx
@@ -0,0 +1,6 @@
+export default function DeactivatedInputContainer() {
+	return (
+		<div className={"group w-full h-36 border-2 rounded-md flex items-center justify-center relative cursor-not-allowed border-zinc-500"}>
+		</div>
+	)
+}
